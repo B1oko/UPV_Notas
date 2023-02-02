20d4222a23d43c3205a54101d129a02c80f2c26c
B1oko <67904023+B1oko@users.noreply.github.com>
2023-02-02 23:35:32 +0100

    mejora y optimizacion del codigo

--- background.js
+++ background.js
@@ -1,79 +1,133 @@
-	
-var table, rows, switching, i, x, y, shouldSwitch;
-table = document.getElementsByClassName("upv_listacolumnas");
-table = table[0] //Seleccionamos la unica tabla que hay
-switching = true;
-while (switching) {
-	switching = false;
-	rows = table.rows;
-	for (i = 1; i < (rows.length - 1); i++) {
-		shouldSwitch = false;
-		x = rows[i].getElementsByTagName("TD")[1];
-		y = rows[i + 1].getElementsByTagName("TD")[1];
-		if (Number(x.innerHTML.replace(',','.')) < Number(y.innerHTML.replace(',','.'))) {
-			shouldSwitch = true;
-			break;
-		}
-	}
-	if (shouldSwitch) {
-		rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-		switching = true;
-	}
-}
 
-// La tabla ya esta ordenada, ahora le aplicamos el formato deseado
-var stats = {aprobados:0,suspendidos:0,media:0};
+// Seleccionamos la tabla
+const tabla = document.getElementsByClassName("upv_listacolumnas")[0];
+const thead = tabla.getElementsByTagName("thead")[0];
+const tbody = tabla.getElementsByTagName("tbody")[0];
+var filas = tbody.rows;
+var estadisticas = { media: 0, aprobados: 0, suspendidos: 0 };
+var listaSeguimiento = [""];
+
+// Modificamos la tabla sustituyendo las comas por puntos y 
+
 try{
 	chrome.storage.sync.get(['text'], function(result) {
 		listaSeguimiento = result.text;
-		if (typeof listaSeguimiento === 'undefined'){listaSeguimiento = ['']}
-		colorear();
-		estadisticas();
+		if (typeof listaSeguimiento === 'undefined'){ listaSeguimiento = [''] }
+		colorearListaSeguimiento();
 	});
-}
-catch(err) {
-	var listaSeguimiento = [''];
-	colorear();
-	estadisticas();
+} catch(err) {
+	console.log(err);
 }
 
-// ---------------------------------------
-// FUNCIONES
-// ---------------------------------------
+// Ordenamos la tabla
+thead.rows[0].insertCell(0).outerHTML = "<th></th>";
+thead.rows[0].getElementsByTagName("th")[1].onclick = function () { ordenarTabla(1); };
+thead.rows[0].getElementsByTagName("th")[2].onclick = function () { ordenarTabla(2); };
 
-function colorear() {
-	table = document.getElementsByClassName("upv_listacolumnas")[0];
-	var contador = 0;
-	var acumulador = 0;
-	rows[0].insertCell(0).outerHTML = "<th></th>"; // Añadimos el titulo, lo añado de esta forma para mantener el formato
-	for (i = 1; i < (rows.length); i++){
-		let fila = rows[i].getElementsByTagName("TD"); //usar esta vatriable
-		contador++;
-		let nota = fila[1].innerHTML = Number(fila[1].innerHTML.replace(',','.'));
-		fila[1].innerHTML = nota; // Modificamos el valor de la tabla para que sea un numero
-		acumulador += nota;
-		if (nota >= 5){stats['aprobados']++}else{stats['suspendidos']++} // Actualizamos las estadisticas
-		rows[i].insertCell(0).innerHTML = contador; // Añadimos la columna con el valor de la posicion
-		rows[i].style.backgroundColor = (contador%2)? 'white' : '#F0F0F0';
-		
+// Por defecto la tabla permanecerá ordenada por nota
+ordenarTabla (2)
+
+// Calculamos las estadísticas
+
+let acumulador = 0;
+
+for (i = 0; i < (filas.length); i++) {
+
+	let nota = Number(filas[i].getElementsByTagName("TD")[2].innerHTML);
+	acumulador += nota;
+	if (nota >= 5) { estadisticas['aprobados']++ } else { estadisticas['suspendidos']++ } // Actualizamos las estadisticas
+}
+
+estadisticas['media'] = acumulador / filas.length;
+
+// Mostramos las estadísticas
+
+const container = document.getElementsByClassName('upv_containerwrap')[0];
+var texto = 'Media: ' + estadisticas['media'].toFixed(3).toString() + '<br>Aprobados: ' + estadisticas['aprobados'].toString() + '<br>Suspensos: ' + estadisticas['suspendidos'].toString();
+var parrafo = document.createElement("p");
+parrafo.innerHTML = texto;
+container.prepend(parrafo);
+
+
+
+function ordenarTabla (n) {
+
+	// Ordenamos la tabla
+
+	let direccion = "desc";
+	let ordenada = true;
+
+	if (filas[0].getElementsByTagName("td").length == 2){
+		for (let i = 0; i < filas.length; i++) {
+			filas[i].insertCell(0).innerHTML = i+1;
+			let columnas = filas[i].getElementsByTagName("td");
+			columnas[2].innerHTML = Number(columnas[2].innerHTML.replace(',', '.'));
+		}
+	}
+
+	for (let i = 0; i < filas.length; i++) {
+		for (let j = 0; j < filas.length - i - 1; j++) {
+
+			let x = filas[j].getElementsByTagName("TD")[n];
+			let y = filas[j + 1].getElementsByTagName("TD")[n];
+
+			//Ordenamos por nombre
+			if (n == 1) {
+				a = x.innerHTML.toLowerCase();
+				b = y.innerHTML.toLowerCase();
+			}
+
+			// Ordenamos por nota
+			else if (n == 2) {
+				a = Number(x.innerHTML);
+				b = Number(y.innerHTML);
+			}
+
+			
+			if (direccion == "asc" && a > b) {
+
+				filas[j].parentNode.insertBefore(filas[j + 1], filas[j]);
+				ordenada = false;
+
+			} else if (direccion == "desc" && a < b) {
+
+				filas[j].parentNode.insertBefore(filas[j + 1], filas[j]);
+				ordenada = false;
+
+			} else if (ordenada && direccion == "desc" && i == 1 ){
+
+				direccion = "asc";
+				i = 0;
+				ordenada = true;
+
+			}
+		}
+	}
+
+	// Añadimos la posicion y coloreamos corectamente
+
+	for (i = 0; i < (filas.length); i++) {
+
+		filas[i].getElementsByTagName("TD")[0].innerHTML = i+1;
+		filas[i].style.backgroundColor = (i % 2) ? 'white' : '#F0F0F0';
+	}
+
+	// Coloreamos usuario en la lista de seguimiento
+	
+	colorearListaSeguimiento();
+
+}
+
+function colorearListaSeguimiento () {
+
+	for (i = 1; i < (filas.length); i++){
 		// Vemos si el nombre esta en la lista de seguimiento
 		for (nombre of listaSeguimiento){
-			if (rows[i].getElementsByTagName("TD")[1].innerHTML == nombre.trim()){ // trim -> eliminamos espacios al inicio y final
-				rows[i].style.backgroundColor = '#8BC34A';
-				rows[i].style.color = 'black';
+			if (filas[i].getElementsByTagName("TD")[1].innerHTML == nombre.trim()){
+				filas[i].style.backgroundColor = '#8BC34A';
+				filas[i].style.color = 'black';
 				break;
 			}
 		}
-		
 	}
-	stats['media'] = acumulador/contador;
-}
-
-function estadisticas() {
-	//Cogemos el container
-	var container = document.getElementsByClassName('upv_containerwrap')[0];
-	var texto = 'Media: '+stats['media'].toFixed(3).toString() + '<br>Aprobados: '+stats['aprobados'].toString() + '<br>Suspensos: '+stats['suspendidos'].toString();
-	var parrafo = document.createElement("p");
-	parrafo.innerHTML = texto;
-	container.prepend(parrafo);
 }
